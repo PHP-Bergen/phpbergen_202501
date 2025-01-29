dc71fede3f0e806649bf2fecf652a05b6a2355f8
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2025-01-29 21:25:03 +0100

    more examples

--- demo_serializer.php
+++ demo_serializer.php
@@ -1,10 +1,15 @@
 <?php
 
 use phpbergen\Attributes\Attributes\Serializer;
+use phpbergen\Attributes\Entities\Product;
 use phpbergen\Attributes\Entities\User;
 
 require 'vendor/autoload.php';
 
 $user = new User('PHP Bergen','php@bergen.no','123456');
 $json = Serializer::serialize($user);
+echo $json . PHP_EOL;
+
+$product = new Product('007','PHP 8.4');
+$json = Serializer::serialize($product);
 echo $json . PHP_EOL;
